ab36a8871600182394d0b20ae96cd83ef6ec63e1
James Andrei Aguilar <jaguilar@gbox.adnu.edu.ph>
2025-07-26 23:54:57 +0800

    fixed bug in profile page

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -3,11 +3,23 @@
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 
+type UserProfile = {
+  full_name: string;
+  email: string;
+  role: string;
+  university_id: number;
+  verification_status: string;
+  created_at: string;
+  universities: {
+    abbreviation: string;
+  };
+};
+
 export default function ProfilePage() {
   const supabase = createClient();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
   const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
 
   useEffect(() => {
     const checkSession = async () => {
@@ -24,15 +36,27 @@ export default function ProfilePage() {
         const { data, error } = await supabase
           .from("users")
           .select(
-            `full_name, email, role, university_id, universities(abbreviation), verification_status, created_at `
+            `
+            full_name,
+            email,
+            role,
+            university_id,
+            verification_status,
+            created_at,
+            universities:universities!users_university_id_fkey (
+              abbreviation
+            )
+          `
           )
           .eq("id", userId)
           .single();
 
+        console.log("Fetched user:", data);
+
         if (error) {
           console.error("Failed to fetch user details: ", error.message);
         } else {
-          setUser(data);
+          setUser(data as UserProfile);
         }
       }
       setLoading(false);
@@ -45,19 +69,23 @@ export default function ProfilePage() {
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex flex-col">
         <h1>Profile Page</h1>
-        {isLoggedIn ? (
+        {loading ? (
+          <p>Loading... </p>
+        ) : isLoggedIn ? (
           <div>
             <p>Name: {user?.full_name}</p>
-            <p>University: {user?.universities.abbreviation}</p>
+            <p>University: {user?.universities?.abbreviation ?? "N/A"}</p>
             <p>Email: {user?.email}</p>
             <p>Role: {user?.role}</p>
             <p>Status: {user?.verification_status}</p>
             <p>
               Joined:{" "}
-              {new Date(user?.created_at).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-              })}
+              {user?.created_at
+                ? new Date(user.created_at).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                  })
+                : "N/A"}
             </p>
           </div>
         ) : (
